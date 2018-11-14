b23928dba25283d2f27a55b6052264fc18deb20d
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-14 15:45:56 -0200

    A  getIp.php

---
+++ getIp.php
@@ -0,0 +1,13 @@
+<?php
+//Anderson Ismael
+//14nov2018
+function getIp(){
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
