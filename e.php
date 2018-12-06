4fbfd9be739fd59bb8f47a505ea8fb8b59c10e8f
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-12-06 16:02:34 -0200

    M  e.php

--- e.php
+++ e.php
@@ -1,6 +1,11 @@
 <?php
 //Criado por Anderson Ismael
 //06 de dezembro de 2018
-function e($str){
-	return htmlentities($str);
+function e($str,$output=true){
+	$str=htmlentities($str);
+	if($output){
+		print $str;
+	}else{
+		return $str;
+	}
 }
