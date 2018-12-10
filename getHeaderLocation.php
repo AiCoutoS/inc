98d4d307f093c8964ea7aa9ae6de348e7de08305
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2018-12-10 20:26:18 -0200

    M  getHeaderLocation.php

--- getHeaderLocation.php
+++ getHeaderLocation.php
@@ -3,7 +3,9 @@
 //05 de outrubro de 2018
 
 function getHeaderLocation($url){
-    $header=get_headers($url,true);
+    if (filter_var($url, FILTER_VALIDATE_URL) !== false) {
+        $header=get_headers($url,true);
+    }
     if(isset($headers['Location'])){
         return $header['Location'];
     }else{
