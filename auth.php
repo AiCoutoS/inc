fa0a802b1852cb32a5062cc0a917699eca08f0e8
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2018-12-11 15:44:07 -0200

    A  auth.php

---
+++ auth.php
@@ -0,0 +1,40 @@
+<?php
+//Anderson Ismael
+//29 de novembro de 2018
+
+function getAuthInstance(){
+    $db=db();
+    return new Basic\Auth($db);
+}
+
+function getUserById($id){
+    $db=db();
+    $where=[
+        'id'=>$id
+    ];
+    return $db->get("users","*",$where);
+}
+
+function isAuth(){
+    $Auth=getAuthInstance();
+    return $Auth->isAuth();
+}
+
+function logout(){
+    $Auth=getAuthInstance();
+    return $Auth->logout();
+}
+
+function signin(){
+    $Auth=getAuthInstance();
+    return $Auth->signin();
+}
+
+function signup($user=false){
+    $Auth=getAuthInstance();
+    if($user){
+        return $Auth->signup($user);
+    }else{
+        return $Auth->signup();
+    }
+}
