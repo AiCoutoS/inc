6203fcd6e0c9d3c2f6315e9c6724496e3057ff1a
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2018-12-21 09:50:34 -0200

    M  model.php

--- model.php
+++ model.php
@@ -15,9 +15,6 @@ function model($names){
 function openModel($name){
     $str=ROOT.'model/'.$name.'.php';
     if(file_exists($str)){
-        if(is_array($data)){
-            extract($data);
-        }
         require_once($str);
     }else{
         die('<b>Erro:</b><br>'.PHP_EOL.'<br>Model <b>'.$name.'</b> não encontrado'.PHP_EOL);
